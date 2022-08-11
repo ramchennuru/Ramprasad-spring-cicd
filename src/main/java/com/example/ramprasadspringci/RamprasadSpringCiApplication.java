3826b8b1e66c893337cf63a61a51654e16bb7ea8
ramchennuru <ramprasad.chennuru@thoughtworks.com>
2022-08-11 13:01:52 +0530

    add src

---
+++ src/main/java/com/example/ramprasadspringci/RamprasadSpringCiApplication.java
@@ -0,0 +1,14 @@
+package com.example.ramprasadspringci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamprasadSpringCiApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        SpringApplication.run(RamprasadSpringCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ramprasadspringci/RamprasadSpringCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramprasadspringci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamprasadSpringCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
