b67656bcde9aa09818b60e464236a58939b49cd0
ramchennuru <ramprasad.chennuru@thoughtworks.com>
2022-08-11 14:15:45 +0530

    print hello

--- src/main/java/com/example/ramprasadspringci/RamprasadSpringCiApplication.java
+++ src/main/java/com/example/ramprasadspringci/RamprasadSpringCiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RamprasadSpringCiApplication {
 
+
     public static void main(String[] args) {
         System.out.println("Hello_Ram");
         SpringApplication.run(RamprasadSpringCiApplication.class, args);
